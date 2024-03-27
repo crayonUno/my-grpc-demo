f12eb6d1ab6daceef3c90c6cf075854e70e60e22
crayon <hexin.in@qq.com>
2024-03-27 16:12:28 +0800

    chore: commit save point

---
+++ src/main/java/org/example/mygrpcdemo/MyGrpcDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.mygrpcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGrpcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGrpcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mygrpcdemo/MyGrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mygrpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGrpcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
